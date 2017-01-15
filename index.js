2d98463b936f483fd4eaa5f24ec3d63e1e32721c
Alistair Brown <github@alistairjcbrown.com>
2017-01-15 20:03:25 +0000

    Push cli success and failure messaging to root index

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 require("babel-register");
+var logger = require('./lib/logger').default;
+
+var fatalError = function(err) {
+  logger.error(err);
+  process.exit(1);
+};
 
 var argv = require('yargs').argv;
-require('./lib').default(argv.webpack, argv.dependency);
+require('./lib').default(argv.webpack, argv.dependency, function(err) {
+  if (err) {
+    fatalError(err);
+  } else {
+    logger.success('All examples passed');
+  }
+});
