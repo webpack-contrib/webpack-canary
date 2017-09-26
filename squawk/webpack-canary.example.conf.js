55d171b2e59644f28bfe7dce37e9931bf923ea07
Darko Kukovec <darko@kukovec.eu>
2017-09-26 19:45:33 +0200

    Working webpack master test

--- squawk/webpack-canary.example.conf.js
+++ squawk/webpack-canary.example.conf.js
@@ -6,13 +6,15 @@ module.exports = {
     }],
     'v2.2.1': [{
       dependency: 'https://github.com/webpack/webpack-dev-server/archive/v2.3.0.tar.gz',
-      exampleDir: 'examples',
+      // exampleDir: 'examples',
       test: 'npm test',
     }],
     'webpack/webpack#master': [{
       dependency: 'https://github.com/webpack/webpack-dev-server/archive/master.tar.gz',
       exampleDir: 'examples',
       test: 'npm test',
+      timeout: 180000,
+      packageManager: 'npm',
     }],
   },
   loglevel: 'trace',
